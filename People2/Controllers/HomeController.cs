f1f0d585213907a1d77caef634519429387398b2
Nikhil <subhaschandn705@gmail.com>
2025-02-22 13:05:27 +0200

    c1

--- People2/Controllers/HomeController.cs
+++ People2/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace People2.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
